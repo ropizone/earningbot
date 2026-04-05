df7d32279a8eb79f6ffa26d580d36b4f4fd63007
ropizone <aryantanvi123@gmail.com>
2026-04-05 10:31:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import (
 )
 
 # ========== CONFIG ==========
-BOT_TOKEN = "PASTE_NEW_TOKEN_HERE"
+BOT_TOKEN = "8663479623:AAEcaM8MXa28bXqTBBh6y9AyH7PhtRHGUWA"
 DEEPSEEK_API_KEY = "sk-d38fc497c0164fc598c28629c573a719"
 ADMIN_PASS = "2010"
 
